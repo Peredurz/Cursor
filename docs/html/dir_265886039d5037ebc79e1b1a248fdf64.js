c023c253d4d81693f2ccb470ff13428f0c576d31
GitHub Action <action@github.com>
2023-04-28 14:15:52 +0000

    deploy files

---
+++ docs/html/dir_265886039d5037ebc79e1b1a248fdf64.js
@@ -0,0 +1,4 @@
+var dir_265886039d5037ebc79e1b1a248fdf64 =
+[
+    [ "IPresentation.cs", "_i_presentation_8cs.html", "_i_presentation_8cs" ]
+];
---
+++ docs/html/dir_7b5b6b265f796ac52538951086374363.js
@@ -0,0 +1,10 @@
+var dir_7b5b6b265f796ac52538951086374363 =
+[
+    [ "DataAccess", "dir_136a5d8ca162bcf06fa1517f01b712dc.html", "dir_136a5d8ca162bcf06fa1517f01b712dc" ],
+    [ "DataModels", "dir_86875eb79c862af77c01565ca630a8fd.html", "dir_86875eb79c862af77c01565ca630a8fd" ],
+    [ "interfaces", "dir_265886039d5037ebc79e1b1a248fdf64.html", "dir_265886039d5037ebc79e1b1a248fdf64" ],
+    [ "Logic", "dir_1378928ac0bc12cc96286fa63dbf1294.html", "dir_1378928ac0bc12cc96286fa63dbf1294" ],
+    [ "obj", "dir_9911fbc54b5f65f254ac8711c6dfa8f4.html", "dir_9911fbc54b5f65f254ac8711c6dfa8f4" ],
+    [ "Presentation", "dir_018407776abadeef67ea7ad98b2e23f4.html", "dir_018407776abadeef67ea7ad98b2e23f4" ],
+    [ "Program.cs", "_program_8cs.html", "_program_8cs" ]
+];
